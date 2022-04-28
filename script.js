579d4e8ec0002135de95a33888a99c2fe90da8e9
AdiRaj94 <adithyarajhemrajan94@gmail.com>
2022-04-28 09:42:01 +0530

    Mobile tab navigation

---
+++ script.js
@@ -0,0 +1,23 @@
+const content = document.querySelectorAll('.content');
+const listItem = document.querySelectorAll('nav ul li');
+
+
+listItem.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        hideAllContents()
+        hideAllItems()
+
+        item.classList.add('active');
+        content[idx].classList.add('show');
+    });
+
+    
+});
+
+function hideAllContents(){
+    content.forEach(content => content.classList.remove('show'))
+}
+
+function hideAllItems(){
+    listItem.forEach(item => item.classList.remove('active'))
+}
